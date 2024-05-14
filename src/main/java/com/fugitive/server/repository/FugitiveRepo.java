34a56138c3126bb037ee10833a627498875f0855
kadir.sarica <kadir.sarica@roboteye.com>
2024-05-14 11:58:35 +0300

    fugitive repo changed

--- src/main/java/com/fugitive/server/repository/FugitiveRepo.java
+++ src/main/java/com/fugitive/server/repository/FugitiveRepo.java
@@ -2,9 +2,13 @@ package com.fugitive.server.repository;
 
 import com.fugitive.server.model.Fugitive;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface FugitiveRepo extends JpaRepository<Fugitive, Integer> {
 
-    List<Fugitive> getFugitivesByColor(String color);
+    @Query("SELECT f FROM Fugitive f WHERE f.color = :color")
+    List<Fugitive> getFugitivesByColor(@Param("color") String color);
 }
