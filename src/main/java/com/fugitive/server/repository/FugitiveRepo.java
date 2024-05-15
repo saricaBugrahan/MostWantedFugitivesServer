84686beb5bc7ff2abab44a8a96d67c8bbb86c299
kadir.sarica <kadir.sarica@roboteye.com>
2024-05-15 07:50:48 +0300

    transactional annotation added into FugitiveRepo

--- src/main/java/com/fugitive/server/repository/FugitiveRepo.java
+++ src/main/java/com/fugitive/server/repository/FugitiveRepo.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface FugitiveRepo extends JpaRepository<Fugitive, Integer> {
-
+   @Transactional
    List<Fugitive> findFugitivesByColor(String color);
 }
