eaebea9232961ac6b20026d9f4c25cee73dc775c
kadir.sarica <kadir.sarica@roboteye.com>
2024-05-20 13:46:14 +0300

    create_table.sql changed

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -1,20 +1,18 @@
-CREATE TABLE IF NOT EXISTS 'fugitives'
-(
-    'id' INT AUTO_INCREMENT PRIMARY KEY,
-    'name' VARCHAR(255) NOT NULL,
-    'surname' VARCHAR(255) NOT NULL,
-    'birthPlace' VARCHAR(255) NOT NULL,
-    'birthDate' VARCHAR(255) NOT NULL,
-    'organization' VARCHAR(255) NOT NULL,
-    'color' VARCHAR(255) NOT NULL,
-    'b64Image' TEXT NOT NULL,
-    'createdDate' TIMESTAMP NOT NULL
-);
+CREATE TABLE IF NOT EXISTS fugitives (
+                                         id SERIAL PRIMARY KEY,
+                                         name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    birthPlace VARCHAR(255) NOT NULL,
+    birthDate VARCHAR(255) NOT NULL,
+    organization VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    b64Image TEXT NOT NULL,
+    createdDate TIMESTAMP NOT NULL
+    );
 
-CREATE TABLE IF NOT EXIST '_user'
-(
-    'id' INT AUTO_INCREMENT PRIMARY KEY,
-    'username' VARCHAR(255) NOT NULL,
-    'password' VARCHAR(255) NOT NULL,
-    'role' VARCHAR(255) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS _user (
+                                     id SERIAL PRIMARY KEY,
+                                     username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL
+    );
